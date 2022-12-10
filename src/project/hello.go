6c21376dc7f4a38863f34d75a68988aa52117908
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-10 09:14:12 +0000

    Create a simple hello-world with go-lang and also print out the linux vagrant env variable

---
+++ src/project/hello.go
@@ -0,0 +1,15 @@
+// Hello Word in Go by Attama Pascal
+package main
+
+// Import OS and fmt packages
+import (
+	"fmt"
+	"os"
+)
+
+
+// Let us start
+func main() {
+    fmt.Println("Hello, world!")  // Print simple text on screen
+    fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable 
+}
