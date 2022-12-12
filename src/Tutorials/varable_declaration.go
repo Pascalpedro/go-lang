90af32e98d7814ec81e015195580f2b761ef43b7
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-12 20:48:30 +0000

    Variable declaration with initial value

---
+++ src/Tutorials/varable_declaration.go
@@ -0,0 +1,12 @@
+package main
+import ("fmt")
+
+func main() {
+  var student1 string = "pascalpedro" //type is string
+  var student2 = "lordpedro" //type is inferred
+  x := 2 //type is inferred
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
+}
