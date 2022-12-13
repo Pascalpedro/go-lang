43c4a53f11c60b2ca8b748cc455e9b3c7fe47200
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-13 05:38:43 +0000

    Declaring variables outside of a function

---
+++ src/Tutorials/variable_declaration4.go
@@ -0,0 +1,15 @@
+// Declaring variables outside of a func...
+
+package main
+import ("fmt")
+
+var a int
+var b int = 2
+var c = 3
+
+func main() {
+  a = 1
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+}
