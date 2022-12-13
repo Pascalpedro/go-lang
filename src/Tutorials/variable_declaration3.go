5cff3ea237f1c7321f40df547b855b62eb24a484
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-13 05:37:59 +0000

    Assigning a value to a variable after it is declared

---
+++ src/Tutorials/variable_declaration3.go
@@ -0,0 +1,10 @@
+// Assigning a value to a variable after it is declared....
+
+package main
+import ("fmt")
+
+func main() {
+  var student1 string
+  student1 = "Pedro"
+  fmt.Println(student1)
+}
