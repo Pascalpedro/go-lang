b43558a20c42c58900f7cedde977e35fe3ef670a
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-15 21:38:26 +0000

    Function call

---
+++ src/Tutorials/function1.go
@@ -0,0 +1,12 @@
+// To call a function...
+
+package main
+import ("fmt")
+
+func myMessage() {
+  fmt.Println("Hei Pascal, I just got executed!")
+}
+
+func main() {
+  myMessage() // call the function
+}
