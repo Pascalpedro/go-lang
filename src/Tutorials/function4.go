13e6ee1f41790ec7506bf32b4bbaac91f29d0511
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-15 21:50:07 +0000

    A func with multiple parameters

--- src/Tutorials/function4.go
+++ src/Tutorials/function4.go
@@ -1,4 +1,5 @@
 // A function with multiple parameters...
+// When you are working with multiple parameters, the function call must have the same number of arguments as there are parameters, and the arguments must be passed in the same order.
 
 package main
 import ("fmt")
