1f64203ecca5af30e9a08149ba03c56441519b33
pascalpedro <Attamapascalpedro@gmail.com>
2022-12-17 21:39:14 +0000

    A regular function with its name

---
+++ src/Tutorials/Anonymous_functions/regular-func.go
@@ -0,0 +1,13 @@
+// A regular function with its name......
+
+package main
+
+import "fmt"
+
+func say_hello(msg string) {
+	fmt.PrintIn(msg)
+}
+
+func main() {
+	say_hello("Hello from a regular function")
+}
